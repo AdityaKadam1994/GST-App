63b2a5402207497d269d142ab754f6426376a937
xoADDYxo <addy.7kad@gmail.com>
2017-10-27 23:22:50 +0530

    Only Splash Screen

---
+++ GST/app/src/main/java/com/example/addy/gst/Menu.java
@@ -0,0 +1,25 @@
+package com.example.addy.gst;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Aditya on 10/27/2017.
+ */
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void onBackPressed() {
+
+
+    }
+
+
+}
